6b69abfc237cbc9153c6c21a4f67f8d73fdf46cd
NadaSaeed168 <36365359+NadaSaeed168@users.noreply.github.com>
2021-04-24 01:11:47 +0200

    Add files via upload

---
+++ LoginContainsAllTestCases.py
@@ -0,0 +1,262 @@
+from selenium import webdriver
+import re
+import random
+import string
+from string import printable
+import time
+
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get('https://www.phptravels.net/register')
+driver.maximize_window()
+
+# first name
+FirstName = driver.find_element_by_name('firstname') #get first name element by name
+randFN = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 7)) #genereate random string of upper and lower letters
+FirstName.send_keys(randFN) #enter the random string in first name
+FirstName.get_attribute('value')
+
+# last name
+LastName = driver.find_element_by_name('lastname') #get last name element by name
+randLN = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 7)) #genereate random string of upper and lower letters
+LastName.send_keys(randLN) #enter the random string in last name
+LastName.get_attribute('value')
+
+# phone number
+phone = driver.find_element_by_name('phone') #get phone element by name
+randNo = str(random.randint(1000000, 9999999)) #generate random phone number
+phone.send_keys(randNo)
+phone.get_attribute('value')
+
+
+# email
+email = driver.find_element_by_name('email') #get email element by name
+randmail = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 7)) #generate random string of 7 digits
+email.send_keys(randmail+''+'@gmail.com') #concatenate the string with @gmail.com
+email.get_attribute('value')
+
+# password
+password = driver.find_element_by_name('password') #get password element by name
+#generate random string of 7 small letters digits
+randPass = ''.join(random.choices(string.ascii_uppercase + string.digits + string.punctuation, k = 7)) #generate random string less than 5 digits
+password.send_keys(randPass) #enter password in text field
+passvalue = password.get_attribute('value')
+
+
+# confirm password
+confirmpassword = driver.find_element_by_name('confirmpassword')
+confirmpassword.send_keys(randPass) #enter the same password entered before in password text field
+
+time.sleep(5) #sleep before submit
+
+#submit
+submit = driver.find_element_by_xpath('/html/body/div[2]/div[1]/section/div/div/div[2]/div/form/div[8]/button')
+submit.click()
+
+time.sleep(15)
+
+#if the image in the page appears after submit successfully appears, take screenshot
+if driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div/div/div[1]/div/div[1]/img'):
+    driver.save_screenshot('failed_password_of_small_letters.png')
+
+# write in execution report, that this test case failed
+f = open('LoginPasswordSmallLetters.txt' , 'w')
+f.write('LoginPasswordSmallLetters' +'\n')
+f.write('Failed Test Case.')
+
+
+driver.close() #close window
+
+
+
+
+##################################Test case tests firstname equal to last name#####################
+driver.close() #close window
+
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get('https://www.phptravels.net/register')
+driver.maximize_window()
+
+FirstName = driver.find_element_by_name('firstname')
+FirstName.send_keys(randFN) #enter the random string in first name
+FirstName.get_attribute('value')
+
+LastName = driver.find_element_by_name('lastname') #get last name element by name
+LastName.send_keys(randFN) #enter same string of first name in last name
+LastName.get_attribute('value')
+
+phone = driver.find_element_by_name('phone') #get phone element by name
+phone.send_keys(randNo)
+phone.get_attribute('value')
+
+email = driver.find_element_by_name('email') #get email element by name
+email.send_keys(randmail+''+'@gmail.com') #concatenate the string with @gmail.com
+email.get_attribute('value')
+
+password = driver.find_element_by_name('password') #get password element by name
+password.send_keys(randPass) #enter password in text field
+passvalue = password.get_attribute('value')
+
+confirmpassword = driver.find_element_by_name('confirmpassword')
+confirmpassword.send_keys(randPass) #enter the same password entered before in password text field
+
+time.sleep(5) #sleep before submit
+
+submit = driver.find_element_by_xpath('/html/body/div[2]/div[1]/section/div/div/div[2]/div/form/div[8]/button')
+submit.click()
+
+time.sleep(15)
+
+#if the image in the page appears after submit successfully appears, take screenshot
+if driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div/div/div[1]/div/div[1]/img'):
+    driver.save_screenshot('failed_first_name_and_last_name_are_equal.png')
+
+# write in execution report, that this test case failed
+f = open('LoginFirstNameLastNameEqual.txt' , 'w')
+f.write('LoginFirstNameLastNameEqual' +'\n')
+f.write('Failed Test Case.')
+
+##################################Test case tests password more than 32 characters#####################
+driver.close() #close window
+
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get('https://www.phptravels.net/register')
+driver.maximize_window()
+
+FirstName = driver.find_element_by_name('firstname')
+FirstName.send_keys(randFN) #enter the random string in first name
+FirstName.get_attribute('value')
+
+LastName = driver.find_element_by_name('lastname') #get last name element by name
+LastName.send_keys(randLN) #enter same string of first name in last name
+LastName.get_attribute('value')
+
+phone = driver.find_element_by_name('phone') #get phone element by name
+phone.send_keys(randNo)
+phone.get_attribute('value')
+
+email = driver.find_element_by_name('email') #get email element by name
+email.send_keys(randmail+''+'@gmail.com') #concatenate the string with @gmail.com
+email.get_attribute('value')
+
+password = driver.find_element_by_name('password') #get password element by name
+randPassMoreThan32 = ''.join(random.choices(string.ascii_uppercase + string.digits + string.punctuation, k = 33)) #generate random string less than 5 digits
+password.send_keys(randPassMoreThan32) #enter password in text field
+passvalue = password.get_attribute('value')
+
+confirmpassword = driver.find_element_by_name('confirmpassword')
+confirmpassword.send_keys(randPassMoreThan32) #enter the same password entered before in password text field
+
+time.sleep(5) #sleep before submit
+
+submit = driver.find_element_by_xpath('/html/body/div[2]/div[1]/section/div/div/div[2]/div/form/div[8]/button')
+submit.click()
+
+time.sleep(15)
+
+#if the image in the page appears after submit successfully appears, take screenshot
+if driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div/div/div[1]/div/div[1]/img'):
+    driver.save_screenshot('failed_password_more_than_32.png')
+
+# write in execution report, that this test case failed
+f = open('LoginPasswordMoreThan32.txt' , 'w')
+f.write('LoginPasswordMoreThan32' +'\n')
+f.write('Failed Test Case.')
+
+
+
+
+##################################Test case tests first name and last name are small letters#####################
+driver.close() #close window
+
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get('https://www.phptravels.net/register')
+driver.maximize_window()
+
+FirstName = driver.find_element_by_name('firstname')
+randFN = ''.join(random.choices(string.ascii_lowercase + string.digits, k = 7)) #genereate random string of lower letters
+FirstName.send_keys(randFN) #enter the random string in first name
+FirstName.get_attribute('value')
+
+LastName = driver.find_element_by_name('lastname') #get last name element by name
+randLN = ''.join(random.choices(string.ascii_lowercase + string.digits, k = 7)) #genereate random string of lower letters
+LastName.send_keys(randLN) #enter same string of first name in last name
+LastName.get_attribute('value')
+
+phone = driver.find_element_by_name('phone') #get phone element by name
+phone.send_keys(randNo)
+phone.get_attribute('value')
+
+email = driver.find_element_by_name('email') #get email element by name
+email.send_keys(randmail+''+'@gmail.com') #concatenate the string with @gmail.com
+email.get_attribute('value')
+
+password = driver.find_element_by_name('password') #get password element by name
+randPassMoreThan32 = ''.join(random.choices(string.ascii_uppercase + string.digits + string.punctuation, k = 33)) #generate random string less than 5 digits
+password.send_keys(randPassMoreThan32) #enter password in text field
+passvalue = password.get_attribute('value')
+
+confirmpassword = driver.find_element_by_name('confirmpassword')
+confirmpassword.send_keys(randPassMoreThan32) #enter the same password entered before in password text field
+
+time.sleep(5) #sleep before submit
+
+submit = driver.find_element_by_xpath('/html/body/div[2]/div[1]/section/div/div/div[2]/div/form/div[8]/button')
+submit.click()
+
+time.sleep(15)
+
+#if the image in the page appears after submit successfully appears, take screenshot
+if driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div/div/div[1]/div/div[1]/img'):
+    driver.save_screenshot('failed_small_letters_in_first_and_last_name.png')
+
+# write in execution report, that this test case failed
+f = open('LoginSmallCharacters.txt' , 'w')
+f.write('LoginSmallCharacters' +'\n')
+f.write('Failed Test Case.')
+
+
+
+###########Test Case tests password with less than 6 characters#########################
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get('https://www.phptravels.net/register')
+driver.maximize_window()
+
+FirstName = driver.find_element_by_name('firstname')
+FirstName.send_keys(randFN) #enter the random string in first name
+FirstName.get_attribute('value')
+
+LastName = driver.find_element_by_name('lastname') #get last name element by name
+LastName.send_keys(randLN) #enter the random string in last name
+LastName.get_attribute('value')
+
+phone = driver.find_element_by_name('phone') #get phone element by name
+phone.send_keys(randNo)
+phone.get_attribute('value')
+
+email = driver.find_element_by_name('email') #get email element by name
+email.send_keys(randmail+''+'@gmail.com') #concatenate the string with @gmail.com
+email.get_attribute('value')
+
+password = driver.find_element_by_name('password') #get password element by name
+randPassLessThan6 = ''.join(random.choices(string.ascii_uppercase + string.digits + string.punctuation, k = 5)) #generate random string less than 5 digits
+password.send_keys(randPassLessThan6) #enter password in text field
+passvalue = password.get_attribute('value')
+
+confirmpassword = driver.find_element_by_name('confirmpassword')
+confirmpassword.send_keys(randPassLessThan6) #enter the same password entered before in password text field
+
+time.sleep(5) #sleep before submit
+
+submit = driver.find_element_by_xpath('/html/body/div[2]/div[1]/section/div/div/div[2]/div/form/div[8]/button')
+submit.click()
+
+time.sleep(15)
+
+# if the error appears that password less than 6 letters, take a screenshot
+if driver.find_element_by_xpath('//*[@id="headersignupform"]/div[2]/div'):
+    driver.save_screenshot('passed_password_less_than_6.png')
+
+# write in execution report, that this test case failed
+f = open('LoginPasswordLessThan6.txt' , 'w')
+f.write('LoginPasswordLessThan6' +'\n')
+f.write('Passed Test Case.')
